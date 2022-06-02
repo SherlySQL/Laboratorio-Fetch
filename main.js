7792a4c32bdd62162e648a7b2166ac10113cf97a
SherlySQL <sherly.clarke@gmail.com>
2022-06-01 20:51:56 -0600

    Pintar Nombre, Imagen, Tabla

--- main.js
+++ main.js
@@ -18,3 +18,70 @@ function Verinfo () {
     })
 }
 
+
+
+function Nombre() {
+    let url="https://randomuser.me/api/"
+    let contenido = document.getElementById("rectangulo")
+    let nombre;
+    let miinformacion
+
+    fetch(url) //URL
+    .then(response => response.json()) //Formato sepador por funcion de flecha =>
+    .then(data =>{
+        nombre = data.results[0].name.first
+        
+    miinformacion =`
+        <p> Nombre: "${nombre}"</p> 
+    `
+    contenido.innerHTML=miinformacion //Meter la info en el HTML
+})
+
+}
+
+
+function Foto() {
+    let url="https://randomuser.me/api/"
+    let contenido = document.getElementById("circulo")
+    let imagen;
+    let miinformacion
+
+    fetch(url) //URL
+    .then(response => response.json()) //Formato sepador por funcion de flecha =>
+    .then(data =>{
+        imagen = data.results[0].picture.large
+        
+    miinformacion =`
+        <img src="${imagen}" alt="" class="img-fluid rounded-circle">
+    `
+    contenido.innerHTML=miinformacion //Meter la info en el HTML
+})
+
+}
+
+
+
+
+function Tabla () {
+    let url="https://jsonplaceholder.typicode.com/todos/"
+    let contenidotabla=document.getElementById("contenidotabla")
+    let misdatos;
+    fetch (url)
+    .then (response=>response.json())
+    .then (data=>{
+
+        data.forEach(element => {
+            misdatos += `
+            <tr>
+                <td>${element.id}</td>
+                <td>${element.nombre}</td>
+                <td>${element.email}</td>
+                <td>${element.estado ? "Activo" : "Inactivo"}</td>  
+            </tr> `    
+        });
+    
+        contenidotabla.innerHTML=misdatos //Meter la info en el HTML
+                                          //? "True" : "Falso" ESTO ES UN IF MODERNO, (si es True cambiar a Activo)
+                                          //(si es False cambiar a Inactivo)     
+    })
+}
